63b96f9d4c5e09dedb43cfd4dab2fa0898683630
Lavinia Paganini <laviniapaganini@gmail.com>
2018-05-31 23:23:02 -0300

    Starting the website

---
+++ hackapet/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './MOSHED-2018-2-18-11-27-45.gif';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} alt="logo"/>
+        <p className="App-title">
+          Em breve.
+        </p>
+        </div>
+    );
+  }
+}
+
